9a7c639cec508a842ab8290df33cfb39e7199453
Sanlee27 <010121qqq@naver.com>
2023-05-27 20:31:40 +0900

    메소드변경_teacher 1명 상세보기

--- src/main/java/dao/TeacherDao.java
+++ src/main/java/dao/TeacherDao.java
@@ -25,7 +25,7 @@ public class TeacherDao {
 		Connection conn = dbUtil.getConnection();
 		// pre
 		// nvl > 널처리 함수. data 값이 null 값일때 임의설정값으로 처리해주기
-		PreparedStatement stmt = conn.prepareStatement("SELECT t.teacher_no tNo, t.teacher_id tId, t.teacher_name tName, nvl(GROUP_CONCAT(s.subject_name), ' ') sName FROM teacher t LEFT OUTER JOIN teacher_subject ts ON t.teacher_no = ts.teacher_no LEFT OUTER JOIN subject s ON ts.subject_no = s.subject_no GROUP BY t.teacher_no, t.teacher_id, t.teacher_name LIMIT ?,?");
+		PreparedStatement stmt = conn.prepareStatement("SELECT t.teacher_no tNo, t.teacher_id tId, t.teacher_name tName, nvl(GROUP_CONCAT(s.subject_name), '-') sName FROM teacher t LEFT OUTER JOIN teacher_subject ts ON t.teacher_no = ts.teacher_no LEFT OUTER JOIN subject s ON ts.subject_no = s.subject_no GROUP BY t.teacher_no, t.teacher_id, t.teacher_name LIMIT ?,?");
 		stmt.setInt(1, beginRow);
 		stmt.setInt(2, rowPerPage);
 		// rs.set
@@ -72,13 +72,13 @@ public class TeacherDao {
 		ResultSet rs = stmt.executeQuery();
 		// 데이터
 		if(rs.next()) {
-			Teacher tBoard = new Teacher();
-			tBoard.setTeacherNo(rs.getInt("tNo"));
-			tBoard.setTeacherId(rs.getString("tId"));
-			tBoard.setTeacherName(rs.getString("tName"));
-			tBoard.setTeacherHistory(rs.getString("tHistory"));
-			tBoard.setCreatedate(rs.getString("createdate"));
-			tBoard.setUpdatedate(rs.getString("updatedate"));
+			teacher= new Teacher();
+			teacher.setTeacherNo(rs.getInt("tNo"));
+			teacher.setTeacherId(rs.getString("tId"));
+			teacher.setTeacherName(rs.getString("tName"));
+			teacher.setTeacherHistory(rs.getString("tHistory"));
+			teacher.setCreatedate(rs.getString("createdate"));
+			teacher.setUpdatedate(rs.getString("updatedate"));
 		}
 		
 		return teacher;
